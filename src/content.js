e9a5297894f4eecde84599c9ae511aaf84ae554e
Subaru Shoji <subaru.shoji@mail.topgate.co.jp>
2021-02-18 21:55:02 +0900

    feat: add link to repository page. refactor: code structure.

--- src/content.js
+++ src/content.js
@@ -4,30 +4,84 @@ window.onload = function () {
     sender,
     sendResponse
   ) {
-    if (request == "addLinkToSuiteCase") {
-      addLinkToSuiteCase();
+    if (request == "addLink") {
+      handleOnTapIcon();
     }
   });
+
   window.addEventListener("click", function () {
-    setTimeout(addLinkToSuiteCase, 100);
+    handlePageChange();
   });
+
+  handleOnLoad();
 };
 
-function addLinkToSuiteCase() {
-  const suitecaseParamsList = Array.from(
+function handleOnTapIcon() {
+  const url = getActiveTabUrl();
+  if (url.includes("plan")) {
+    addLinkToSuiteCaseInTestPlan();
+  } else if (url.includes("project")) {
+    addLinkToCaseInRepository();
+  }
+}
+
+async function handlePageChange() {
+  const url = getActiveTabUrl();
+
+  if (url.includes("plan")) {
+    await sleep(1000);
+    addLinkToSuiteCaseInTestPlan();
+  } else if (url.includes("project")) {
+    await sleep(6000);
+    addLinkToCaseInRepository();
+  }
+}
+
+async function handleOnLoad() {
+  const url = getActiveTabUrl();
+  if (url.includes("project")) {
+    await sleep(6000);
+    addLinkToCaseInRepository();
+  }
+}
+
+function getActiveTabUrl() {
+  return location.href;
+}
+
+function sleep(milliseconds) {
+  return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+function addLinkToSuiteCaseInTestPlan() {
+  const suitecaseParamsElementList = Array.from(
     document.getElementsByClassName("suitecase-params")
   );
-  suitecaseParamsList.forEach((suiteCaseParams) => {
-    const suiteKeyElement = suiteCaseParams.children[0];
+  suitecaseParamsElementList.forEach((suiteCaseParamsElement) => {
+    const suiteKeyElement = suiteCaseParamsElement.children[0];
 
-    const suiteKey = suiteKeyElement.innerHTML;
-    if (suiteKey[0] === "<") {
+    const suiteCaseId = suiteKeyElement.innerHTML;
+    if (suiteCaseId[0] === "<") {
       return;
     }
 
-    const [projectId, suiteId] = suiteKey.split("-");
-    const suiteLink = `https://app.qase.io/case/${projectId}/edit/${suiteId}`;
+    const [projectId, caseId] = suiteCaseId.split("-");
+    const caseLink = `https://app.qase.io/case/${projectId}/edit/${caseId}`;
 
-    suiteKeyElement.innerHTML = `<a href="${suiteLink}" target="_blank">${suiteKey}</a>`;
+    suiteKeyElement.innerHTML = `<a href="${caseLink}" target="_blank">${suiteCaseId}</a>`;
+  });
+}
+
+function addLinkToCaseInRepository() {
+  const caseRowIdElementList = Array.from(
+    document.getElementsByClassName("case-row-id")
+  );
+
+  caseRowIdElementList.forEach((caseRowIdElement) => {
+    const suiteCaseId = caseRowIdElement.innerHTML;
+    const [projectId, caseId] = suiteCaseId.split("-");
+    const caseLink = `https://app.qase.io/case/${projectId}/edit/${caseId}`;
+
+    caseRowIdElement.innerHTML = `<a href="${caseLink}" target="_blank">${suiteCaseId}</a>`;
   });
 }
