9a3268d1b55a27441a7a9ae7dac1c76f006541e2
Subaru Shoji <shoji.subaru@gmail.com>
2021-06-16 16:01:47 +0900

    fix: いろいろ動かなくなってたので全部修正。すでに適用されていたら中断する処理を実装。

--- src/content.js
+++ src/content.js
@@ -5,39 +5,30 @@ window.onload = function () {
     sendResponse
   ) {
     if (request == "addLink") {
-      handleOnTapIcon();
+      applyChanges(false);
     }
   });
 
   window.addEventListener("click", function () {
-    handlePageChange();
+    applyChanges(true);
   });
 
-  handleOnLoad();
+  applyChanges(true);
 };
 
-function handleOnTapIcon() {
+function applyChanges(withSleep) {
   const url = getActiveTabUrl();
   if (url.includes("plan")) {
+    if (withSleep) {
+      sleep(1000);
+    }
+
     addLinkToSuiteCaseInTestPlan();
   } else if (url.includes("project")) {
-    addLinkToCaseInRepository();
-  }
-}
+    if (withSleep) {
+      sleep(6000);
+    }
 
-async function handlePageChange() {
-  const url = getActiveTabUrl();
-
-  if (url.includes("project")) {
-    await sleep(6000);
-    addLinkToCaseInRepository();
-  }
-}
-
-async function handleOnLoad() {
-  const url = getActiveTabUrl();
-  if (url.includes("project")) {
-    await sleep(6000);
     addLinkToCaseInRepository();
   }
 }
@@ -54,19 +45,20 @@ function addLinkToSuiteCaseInTestPlan() {
   const suitecaseParamsElementList = Array.from(
     document.getElementsByClassName("suitecase-params")
   );
-  suitecaseParamsElementList.forEach((suiteCaseParamsElement) => {
+
+  for (var suiteCaseParamsElement of suitecaseParamsElementList) {
     const suiteKeyElement = suiteCaseParamsElement.children[0];
 
     const suiteCaseId = suiteKeyElement.innerHTML;
     if (suiteCaseId[0] === "<") {
-      return;
+      break;
     }
 
     const [projectId, caseId] = suiteCaseId.split("-");
     const caseLink = `https://app.qase.io/case/${projectId}/edit/${caseId}`;
 
     suiteKeyElement.innerHTML = `<a href="${caseLink}" target="_blank">${suiteCaseId}</a>`;
-  });
+  }
 }
 
 function addLinkToCaseInRepository() {
@@ -74,11 +66,16 @@ function addLinkToCaseInRepository() {
     document.getElementsByClassName("case-row-id")
   );
 
-  caseRowIdElementList.forEach((caseRowIdElement) => {
+  for (var caseRowIdElement of caseRowIdElementList) {
     const suiteCaseId = caseRowIdElement.innerHTML;
+
+    if (suiteCaseId[0] === "<") {
+      break;
+    }
+
     const [projectId, caseId] = suiteCaseId.split("-");
     const caseLink = `https://app.qase.io/case/${projectId}/edit/${caseId}`;
 
     caseRowIdElement.innerHTML = `<a href="${caseLink}" target="_blank">${suiteCaseId}</a>`;
-  });
+  }
 }
